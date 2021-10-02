c3415fff5892dec2aa54597262d1502267821540
dronifion <dronifion@gmail.com>
2021-10-02 19:39:16 +0800

    comment

--- src/wikipediaSearch.py
+++ src/wikipediaSearch.py
@@ -2,34 +2,34 @@ import wikipedia as wiki
 import kivy
 from kivy.app import App
 #from kivy.lang import Builder # it is to import Builder
-from kivy.uix.label import Label
+#from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.progressbar import ProgressBar 
+#from kivy.uix.textinput import TextInput
+#from kivy.uix.button import Button
+#from kivy.uix.progressbar import ProgressBar 
 
-class mainPage(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 2
+# class mainPage(GridLayout):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.cols = 2
 
-        self.add_widget(Label(text="Question: "))
-        self.question = TextInput(multiline=False)
-        self.add_widget(self.question)
+#         self.add_widget(Label(text="Question: "))
+#         self.question = TextInput(multiline=False)
+#         self.add_widget(self.question)
 
-        self.add_widget(Label(text="Answer: "))
-        self.answer = Label(text="", id="inputAnswerId")
-        self.add_widget(self.answer)
+#         self.add_widget(Label(text="Answer: "))
+#         self.answer = Label(text="", id="inputAnswerId")
+#         self.add_widget(self.answer)
 
-        self.submit = Button(text="Submit")
-        self.submit.bind(on_press=root.onPress_submitButton)
-        self.add_widget(self.submit)
+#         self.submit = Button(text="Submit")
+#         self.submit.bind(on_press=root.onPress_submitButton)
+#         self.add_widget(self.submit)
 
-    def onPress_submitButton(self, instance):
-        q = self.question.text
-        a_wiki = wiki.summary(str(q))
-        print(a_wiki)
-        instance.parent.ids.inputAnswerId.text = a_wiki
+#     def onPress_submitButton(self, instance):
+#         q = self.question.text
+#         a_wiki = wiki.summary(str(q))
+#         print(a_wiki)
+#         instance.parent.ids.inputAnswerId.text = a_wiki
 
 # building kv file as string
 #kvfile = Builder.load_string("""
